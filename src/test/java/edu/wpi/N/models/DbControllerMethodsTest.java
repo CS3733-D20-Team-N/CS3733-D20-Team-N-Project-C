460cfba1bcf0ee3d8104a2f9454eb4bf8238f066
kofichtner <kofichtner@wpi.edu>
2020-04-11 22:24:29 -0400

    adds tests for new database implementation of pathfinding

--- src/test/java/edu/wpi/N/models/DbControllerMethodsTest.java
+++ src/test/java/edu/wpi/N/models/DbControllerMethodsTest.java
@@ -1,11 +1,15 @@
 package edu.wpi.N.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import edu.wpi.N.Main;
 import edu.wpi.N.database.dbController;
 import java.io.InputStream;
 import java.sql.SQLException;
+import java.util.LinkedList;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -35,137 +39,127 @@ public class DbControllerMethodsTest {
   //  Pathfinder newPath = new Pathfinder(testGraph, startNode, endNode);
   //  Path myPath = newPath.findPath();
   //
-  //  /** Tests that getEdges(nodeID) returns the correct list of edges for a given node */
-  //  @Test
-  //  public void getEdgesTester() {
-  //    LinkedList<String> hall1Edges = new LinkedList<String>();
-  //    hall1Edges.add("MOHSClinic");
-  //    hall1Edges.add("HALL2");
-  //    Assertions.assertEquals(testGraph.getEdges("HALL1"), hall1Edges);
-  //
-  //    LinkedList<String> hall3Edges = new LinkedList<String>();
-  //    hall3Edges.add("HALL2");
-  //    hall3Edges.add("HALL4");
-  //    Assertions.assertEquals(testGraph.getEdges("HALL3"), hall3Edges);
-  //
-  //    LinkedList<String> neurologyEdges = new LinkedList<String>();
-  //    neurologyEdges.add("HALL6");
-  //    Assertions.assertEquals(testGraph.getEdges("Neurology"), neurologyEdges);
-  //  }
-  //
-  //  /**
-  //   * Tests that getEdges(nodeID) will return null if the node is not in the graph and if the
-  // node
-  //   * does not have any edges
-  //   */
-  //  @Test
-  //  public void getEdgesNullTester() {
-  //    Node testNode = new Node(2.345, 5.5657, "TESTNODE");
-  //    assertNull(testGraph.getEdges("TESTNODE"));
-  //
-  //    testGraph.addNode(testNode);
-  //    assertNull(testGraph.getEdges("TESTNODE"));
-  //  }
-  //
-  //  /**
-  //   * Tests that addEdges(nodeID1,nodeID2) will add the given edge to the list of nodes for both
-  //   * given nodes
-  //   */
-  //  @Test
-  //  public void addEdgesTester() {
-  //    LinkedList<String> hall1Edges = new LinkedList<String>();
-  //    hall1Edges.add("MOHSClinic");
-  //    hall1Edges.add("HALL2");
-  //    hall1Edges.add("HALL3");
-  //    testGraph.addEdge("HALL1", "HALL3");
-  //    Assertions.assertEquals(testGraph.getEdges("HALL1"), hall1Edges);
-  //
-  //    LinkedList<String> hall3Edges = new LinkedList<String>();
-  //    hall3Edges.add("HALL2");
-  //    hall3Edges.add("HALL4");
-  //    hall3Edges.add("HALL1");
-  //    Assertions.assertEquals(testGraph.getEdges("HALL3"), hall3Edges);
-  //  }
-  //
-  //  /**
-  //   * Tests that addEdges(nodeID1,nodeID2) will create a list of edges for a node that currently
-  // has
-  //   * no edges
-  //   */
-  //  @Test
-  //  public void addEdgesEmptyNodeTester() {
-  //    testGraph.addEdge("TESTNODE", "Elevator");
-  //    LinkedList<String> testNodeEdges = new LinkedList<String>();
-  //    testNodeEdges.add("Elevator");
-  //    Assertions.assertEquals(testGraph.getEdges("TESTNODE"), testNodeEdges);
-  //  }
-  //
-  //  /**
-  //   * Tests that a duplicate edge will not be added if you run addEdge(nodeID1,nodeID2) on an
-  // edge
-  //   * that already exists
-  //   */
-  //  @Test
-  //  public void addEdgeAlreadyThereTester() {
-  //    LinkedList<String> neurologyEdges = new LinkedList<String>();
-  //    neurologyEdges.add("HALL6");
-  //    LinkedList<String> hall6Edges = new LinkedList<String>();
-  //    hall6Edges.add("HALL5");
-  //    hall6Edges.add("HALL7");
-  //    hall6Edges.add("Neurology");
-  //    testGraph.addEdge("HALL6", "Neurology");
-  //    Assertions.assertEquals(testGraph.getEdges("Neurology"), neurologyEdges);
-  //    Assertions.assertEquals(testGraph.getEdges("HALL6"), hall6Edges);
-  //  }
-  //
-  //  /** Tests that heuristic(currNode, endNode) returns the correct calculated value */
-  //  @Test
-  //  public void heuristicTester() {
-  //    Assertions.assertEquals(
-  //        newPath.heuristic(testGraph.getNode("MOHSClinic"), testGraph.getNode("HALL1")),
-  //        0.77,
-  //        0.0001);
-  //  }
-  //
-  //  /**
-  //   * Tests that heuristic(currNode, endNode) returns the correct calculated value for a node
-  // that is
-  //   * not in the graph
-  //   */
-  //  @Test
-  //  public void heuristicNotInGraphTester() {
-  //    Node testNode1 = new Node(1, 0, "TESTNODE1");
-  //    Node testNode2 = new Node(0, 1, "TESTNODE2");
-  //    Assertions.assertEquals(newPath.heuristic(testNode1, testNode2), 2, 0.0001);
-  //  }
-  //
-  //  /**
-  //   * Tests that getNode(nodeID) returns the correct node when given a nodeID that is in the
-  // graph
-  //   */
-  //  @Test
-  //  public void getNodeTester() {
-  //    Node testNode = new Node(5.762, 0.646, "MOHSClinic");
-  //    Assertions.assertEquals(testGraph.getNode("MOHSClinic"), testNode);
-  //
-  //    Node testNode2 = new Node(6.532, 4.562, "HALL10");
-  //    Assertions.assertEquals(testGraph.getNode("HALL10"), testNode2);
-  //  }
-  //
-  //  /**
-  //   * Tests that getNode(nodeID) returns null when given an nodeID that isn't in the graph or
-  // doesn't
-  //   * exist at all
-  //   */
-  //  @Test
-  //  public void getNodeNullTester() {
-  //    // Call getNode on node that doesn't exist at all
-  //    assertNull(testGraph.getNode("test1"));
-  //
-  //    // Call getNode on node that exists but not isn't in the graph
-  //    Node testNode2 = new Node(6.5, 2.0, "test2");
-  //    assertNull(testGraph.getNode("test2"));
-  //  }
+   /** Tests that getEdges(nodeID) returns the correct list of edges for a given node */
+   @Test
+  public void getEdgesTester() {
+  LinkedList<Node> hall1Edges = new LinkedList<Node>();
+  hall1Edges.add(dbController.getGNode("MOHSClinic"));
+  hall1Edges.add(dbController.getGNode("HALL2"));
+  Assertions.assertEquals(dbController.getGAdjacent("HALL1"), hall1Edges);
+
+  LinkedList<Node> hall3Edges = new LinkedList<Node>();
+  hall3Edges.add(dbController.getGNode("HALL2"));
+  hall3Edges.add(dbController.getGNode("HALL4"));
+  Assertions.assertEquals(dbController.getGAdjacent("HALL3"), hall3Edges);
+
+  LinkedList<Node> neurologyEdges = new LinkedList<Node>();
+  neurologyEdges.add(dbController.getGNode("HALL6"));
+  Assertions.assertEquals(dbController.getGAdjacent("Neurology"), neurologyEdges);
+  }
+
+  /**
+  * Tests that getEdges(nodeID) will return null if the node is not in the graph and if the node
+  * does not have any edges
+  */
+  @Test
+  public void getEdgesNullTester() throws SQLException {
+  Node testNode = new Node(2.345, 5.5657, "TESTNODE2");
+  assertNull(dbController.getGAdjacent("TESTNODE2"));
+
+  dbController.addNode("TESTNODE2", 23, 345, 4, "Foisie", "sdfkjd", "fskjd", "sdfk", 'N');
+  assertNull(dbController.getGAdjacent("TESTNODE2"));
+  }
+
+  /**
+  * Tests that addEdges(nodeID1,nodeID2) will add the given edge to the list of nodes for both
+  * given nodes
+  */
+  @Test
+  public void addEdgesTester() {
+  LinkedList<Node> hall1Edges = new LinkedList<Node>();
+  hall1Edges.add(dbController.getGNode("MOHSClinic"));
+  hall1Edges.add(dbController.getGNode("HALL2"));
+  hall1Edges.add(dbController.getGNode("HALL3"));
+  dbController.addEdge("HALL1", "HALL3");
+  Assertions.assertEquals(dbController.getGAdjacent("HALL1"), hall1Edges);
+
+  LinkedList<Node> hall3Edges = new LinkedList<Node>();
+  hall3Edges.add(dbController.getGNode("HALL2"));
+  hall3Edges.add(dbController.getGNode("HALL4"));
+  hall3Edges.add(dbController.getGNode("HALL1"));
+  Assertions.assertEquals(dbController.getGAdjacent("HALL3"), hall3Edges);
+  }
+
+  /**
+   * Tests that addEdges(nodeID1,nodeID2) will create a list of edges for a node that currently has
+   * no edges
+   */
+  @Test
+  public void addEdgesEmptyNodeTester() {
+   dbController.addEdge("TESTNODE2", "Elevator");
+  LinkedList<Node> testNodeEdges = new LinkedList<Node>();
+  testNodeEdges.add(dbController.getGNode("Elevator"));
+  Assertions.assertEquals(dbController.getGAdjacent("TESTNODE2"), testNodeEdges);
+   }
+
+  /**
+  * Tests that a duplicate edge will not be added if you run addEdge(nodeID1,nodeID2) on an edge
+  * that already exists
+  */
+  @Test
+  public void addEdgeAlreadyThereTester() {
+  LinkedList<Node> neurologyEdges = new LinkedList<Node>();
+  neurologyEdges.add(dbController.getGNode("HALL6"));
+  LinkedList<Node> hall6Edges = new LinkedList<Node>();
+  hall6Edges.add(dbController.getGNode("HALL5"));
+  hall6Edges.add(dbController.getGNode("HALL7"));
+  hall6Edges.add(dbController.getGNode("Neurology"));
+  dbController.addEdge("HALL6", "Neurology");
+  Assertions.assertEquals(dbController.getGAdjacent("Neurology"), neurologyEdges);
+  Assertions.assertEquals(dbController.getGAdjacent("HALL6"), hall6Edges);
+  }
+  /** Tests that heuristic(currNode, endNode) returns the correct calculated value */
+  @Test
+  public void heuristicTester() {
+  Assertions.assertEquals(
+          Pathfinder.heuristic(dbController.getGNode("MOHSClinic"), dbController.getGNode("HALL1")),
+         0.77,
+         0.0001);
+  }
+
+  /**
+  * Tests that heuristic(currNode, endNode) returns the correct calculated value for a node that is
+  * not in the graph
+  */
+  @Test
+  public void heuristicNotInGraphTester() {
+  Node testNode1 = new Node(1, 0, "TESTNODE3");
+  Node testNode2 = new Node(0, 1, "TESTNODE4");
+  Assertions.assertEquals(Pathfinder.heuristic(testNode1, testNode2), 2, 0.0001);
+   }
+
+   /**
+  * Tests that getNode(nodeID) returns the correct node when given a nodeID that is in the graph
+  */
+  @Test
+  public void getNodeTester() {
+  Node testNode = new Node(5.762, 0.646, "MOHSClinic");
+  Assertions.assertEquals(dbController.getGNode("MOHSClinic"), testNode);
+  Node testNode2 = new Node(6.532, 4.562, "HALL10");
+  Assertions.assertEquals(dbController.getGNode("HALL10"), testNode2);
+  }
+  /**
+   * Tests that getNode(nodeID) returns null when given an nodeID that isn't in the graph or doesn't
+   * exist at all
+   */
+   @Test
+  public void getNodeNullTester() {
+  // Call getNode on node that doesn't exist at all
+  assertNull(dbController.getGNode("test1"));
+   // Call getNode on node that exists but not isn't in the graph
+   Node testNode2 = new Node(6.5, 2.0, "test2");
+   assertNull(dbController.getGNode("test2"));
+   }
   //
   //  /** Tests that addNode(node) adds the given node to the graph */
   //  @Test
