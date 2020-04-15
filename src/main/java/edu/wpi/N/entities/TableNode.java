3f660f582494b9c809c9325c33a6804e3a7e44df
Matt Johannesen <matt.d.johannesen@gmail.com>
2020-04-15 10:09:10 -0400

    At long last... edits can be made!

--- src/main/java/edu/wpi/N/entities/TableNode.java
+++ src/main/java/edu/wpi/N/entities/TableNode.java
@@ -1,70 +1,151 @@
 package edu.wpi.N.entities;
 
 import edu.wpi.N.database.DbNode;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.beans.value.ObservableValue;
+import edu.wpi.N.database.dbController;
 
 // Re-maps DbNode data to a format that automatically updates a JavaFX TableViewer node
 public class TableNode {
 
-  private IntegerProperty x = new SimpleIntegerProperty();
-  private IntegerProperty y = new SimpleIntegerProperty();
-  private IntegerProperty floor = new SimpleIntegerProperty();
-  private StringProperty nodeID = new SimpleStringProperty();
-  private StringProperty building = new SimpleStringProperty();
-  private StringProperty nodeType = new SimpleStringProperty();
-  private StringProperty longName = new SimpleStringProperty();
-  private StringProperty shortName = new SimpleStringProperty();
-  private StringProperty teamAssigned = new SimpleStringProperty();
+  private Integer x, y, floor;
+  private String nodeID, building, nodeType, longName, shortName;
+  private Character teamAssigned;
 
   public TableNode(DbNode node) {
-    x.set(node.getX());
-    y.set(node.getY());
-    floor.set(node.getFloor());
-    nodeID.set(node.getNodeID());
-    building.set(node.getBuilding());
-    nodeType.set(node.getNodeType());
-    longName.set(node.getLongName());
-    shortName.set(node.getShortName());
-    teamAssigned.set("" + node.getTeamAssigned());
+    x = node.getX();
+    y = node.getY();
+    floor = node.getFloor();
+    nodeID = node.getNodeID();
+    building = node.getBuilding();
+    nodeType = node.getNodeType();
+    longName = node.getLongName();
+    shortName = node.getShortName();
+    teamAssigned = node.getTeamAssigned();
   }
 
-  public ObservableValue<Number> xProperty() {
+  /**
+   * Gets the x value of the node
+   *
+   * @return the x value of the node
+   */
+  public int getX() {
     return x;
   }
 
-  public ObservableValue<Number> yProperty() {
-    return y;
-  }
-
-  public ObservableValue<Number> floorProperty() {
+  /**
+   * Gets the floor of the node
+   *
+   * @return The node's floor
+   */
+  public int getFloor() {
     return floor;
   }
 
-  public ObservableValue<String> nodeIDProperty() {
+  /**
+   * Gets the node's ID
+   *
+   * @return The ID of the node
+   */
+  public String getNodeID() {
     return nodeID;
   }
 
-  public ObservableValue<String> buildingProperty() {
+  /**
+   * Gets the building of the node
+   *
+   * @return the Node's building
+   */
+  public String getBuilding() {
     return building;
   }
 
-  public ObservableValue<String> nodeTypeProperty() {
+  /**
+   * Gets the type of the node
+   *
+   * @return the node's type
+   */
+  public String getNodeType() {
     return nodeType;
   }
 
-  public ObservableValue<String> longNameProperty() {
+  /**
+   * Gets the long name of the Node
+   *
+   * @return The node's long name
+   */
+  public String getLongName() {
     return longName;
   }
 
-  public ObservableValue<String> shortNameProperty() {
+  /**
+   * Gets the short name of the Node
+   *
+   * @return The node's short name
+   */
+  public String getShortName() {
     return shortName;
   }
 
-  public ObservableValue<String> teamAssignedProperty() {
+  /**
+   * Gets the team assigned to the node
+   *
+   * @return the team assigned to the node
+   */
+  public Character getTeamAssigned() {
     return teamAssigned;
+  }
+
+  /**
+   * Gets the Y value of the node
+   *
+   * @return the node's Y value
+   */
+  public int getY() {
+    return y;
+  }
+
+  public void SetX(Integer newX) {
+    x = newX;
+  }
+
+  public void SetY(Integer newY) {
+    y = newY;
+  }
+
+  public void SetFloor(Integer newFloor) {
+    floor = newFloor;
+  }
+
+  public void SetNodeID(String newID) {
+    nodeID = newID;
+  }
+
+  public void SetBuilding(String newBuilding) {
+    building = newBuilding;
+  }
+
+  public void SetNodeType(String newNodeType) {
+    nodeType = newNodeType;
+  }
+
+  public void SetLongName(String newLongName) {
+    longName = newLongName;
+  }
+
+  public void SetShortName(String newShortName) {
+    shortName = newShortName;
+  }
+
+  public void SetTeamAssigned(String newTeam) {
+    teamAssigned = newTeam.charAt(0);
+  }
+
+  public void SetTeamAssigned(char newTeam) {
+    teamAssigned = newTeam;
+  }
+
+  public void UpdateDbNode() {
+    // Run modifyNode using this node's info
+    dbController.modifyNode(
+        nodeID, x, y, floor, building, nodeType, longName, shortName, teamAssigned);
   }
 }
--- src/main/java/edu/wpi/N/views/NodeTableEditorController.java
+++ src/main/java/edu/wpi/N/views/NodeTableEditorController.java
@@ -8,70 +8,197 @@ import edu.wpi.N.models.CSVParser;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.LinkedList;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.Event;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.util.converter.CharacterStringConverter;
+import javafx.util.converter.IntegerStringConverter;
 
 public class NodeTableEditorController {
-  @FXML Button btn_saveChanges, btn_next, btn_back;
-  @FXML TableView nodesTable;
-  @FXML ChoiceBox menu_pickBuilding, menu_pickFloor; // , menu_pickNode;
+  @FXML Button btn_saveChanges, btn_next, btn_back, btn_addNode, btn_delNode;
+  @FXML TableView<TableNode> nodesTable;
+  @FXML ComboBox<String> menu_pickBuilding;
+  @FXML ComboBox<Integer> menu_pickFloor;
+  @FXML Label lbl_changesSaved;
   private LinkedList<String> buildings = new LinkedList<>();
   private LinkedList<TableNode> allNodes = new LinkedList<>();
-  private LinkedList<Number> floors = new LinkedList<>();
+  private LinkedList<Integer> floors = new LinkedList<>();
   private ObservableList<TableNode> currentNodes = FXCollections.observableArrayList();
-  private int currentFloor;
+  private LinkedList<TableNode> editedNodes =
+      new LinkedList<>(); // Stores field changes until database is saved
   private String currentBuilding;
 
   @FXML
   private void initialize() throws java.io.IOException {
-    InputStream prototype = Main.class.getResourceAsStream("csv/PrototypeNodes.csv");
-    CSVParser.parseCSV(prototype);
 
-    System.out.println(dbController.allNodes().size());
+    // Set up table column parameters
 
+    TableColumn<TableNode, String> nameCol = new TableColumn<>("Name");
+    TableColumn<TableNode, String> longNameCol = new TableColumn<>("Long Name");
+    TableColumn<TableNode, String> idCol = new TableColumn<>("Node ID");
+    TableColumn<TableNode, String> typeCol = new TableColumn<>("Type");
+    TableColumn<TableNode, Integer> xCol = new TableColumn<>("X");
+    TableColumn<TableNode, Integer> yCol = new TableColumn<>("Y");
+    TableColumn<TableNode, Integer> floorCol = new TableColumn<>("Floor");
+    TableColumn<TableNode, String> buildingCol = new TableColumn<>("Building");
+    TableColumn<TableNode, Character> teamCol = new TableColumn<>("Team");
+
+    // Set columns
+
+    nameCol.setCellValueFactory(new PropertyValueFactory<>("ShortName"));
+    longNameCol.setCellValueFactory(new PropertyValueFactory<>("LongName"));
+    idCol.setCellValueFactory(new PropertyValueFactory<>("NodeID"));
+    typeCol.setCellValueFactory(new PropertyValueFactory<>("NodeType"));
+    xCol.setCellValueFactory(new PropertyValueFactory<>("X"));
+    yCol.setCellValueFactory(new PropertyValueFactory<>("Y"));
+    floorCol.setCellValueFactory(new PropertyValueFactory<>("Floor"));
+    buildingCol.setCellValueFactory(new PropertyValueFactory<>("Building"));
+    teamCol.setCellValueFactory(new PropertyValueFactory<>("TeamAssigned"));
+
+    // Set functions to be called when fields are edited
+
+    nameCol.setCellFactory(TextFieldTableCell.forTableColumn());
+    nameCol.setOnEditCommit(
+        (TableColumn.CellEditEvent<TableNode, String> t) -> {
+          t.getRowValue().SetShortName(t.getNewValue());
+          cacheEdit(t.getRowValue());
+        });
+
+    longNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
+    longNameCol.setOnEditCommit(
+        (TableColumn.CellEditEvent<TableNode, String> t) -> {
+          t.getRowValue().SetLongName(t.getNewValue());
+          cacheEdit(t.getRowValue());
+        });
+
+    idCol.setCellFactory(TextFieldTableCell.forTableColumn());
+    idCol.setOnEditCommit(
+        (TableColumn.CellEditEvent<TableNode, String> t) -> {
+          t.getRowValue().SetNodeID(t.getNewValue());
+          cacheEdit(t.getRowValue());
+        });
+
+    typeCol.setCellFactory(TextFieldTableCell.forTableColumn());
+    typeCol.setOnEditCommit(
+        (TableColumn.CellEditEvent<TableNode, String> t) -> {
+          t.getRowValue().SetNodeType(t.getNewValue());
+          cacheEdit(t.getRowValue());
+        });
+
+    xCol.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+    xCol.setOnEditCommit(
+        (TableColumn.CellEditEvent<TableNode, Integer> t) -> {
+          t.getRowValue().SetX(t.getNewValue());
+          cacheEdit(t.getRowValue());
+        });
+
+    yCol.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+    yCol.setOnEditCommit(
+        (TableColumn.CellEditEvent<TableNode, Integer> t) -> {
+          t.getRowValue().SetY(t.getNewValue());
+          cacheEdit(t.getRowValue());
+        });
+
+    floorCol.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+    floorCol.setOnEditCommit(
+        (TableColumn.CellEditEvent<TableNode, Integer> t) -> {
+          t.getRowValue().SetFloor(t.getNewValue());
+          cacheEdit(t.getRowValue());
+        });
+
+    buildingCol.setCellFactory(TextFieldTableCell.forTableColumn());
+    buildingCol.setOnEditCommit(
+        (TableColumn.CellEditEvent<TableNode, String> t) -> {
+          t.getRowValue().SetBuilding(t.getNewValue());
+          cacheEdit(t.getRowValue());
+        });
+
+    teamCol.setCellFactory(TextFieldTableCell.forTableColumn(new CharacterStringConverter()));
+    teamCol.setOnEditCommit(
+        (TableColumn.CellEditEvent<TableNode, Character> t) -> {
+          t.getRowValue().SetTeamAssigned(t.getNewValue());
+          cacheEdit(t.getRowValue());
+        });
+
+    // Place columns
+    nodesTable
+        .getColumns()
+        .setAll(nameCol, longNameCol, idCol, typeCol, xCol, yCol, floorCol, buildingCol, teamCol);
+
+    // Load PrototypeNodes CSV file as per Part C instructions
+    InputStream nodesToAdd = Main.class.getResourceAsStream("csv/PrototypeNodes.csv");
+
+    // Add nodes from CSV to database
+    CSVParser.parseCSV(nodesToAdd);
+
+    // Get nodes from database and fill table
     refreshAllNodes();
     getBuildings();
     fillTable();
   }
 
+  /// FXML methods
+
+  // Called when user has changed which building they want to look at
   @FXML
-  public void onPickBuilding(MouseEvent event) {
+  public void onPickBuilding(ActionEvent event) {
+    currentBuilding = menu_pickBuilding.getValue();
     getFloorsInBuilding(currentBuilding);
-    getNodesOnFloor(currentBuilding, 1);
-    fillTable();
   }
 
+  // Called when user has changed which floor they want to look at
   @FXML
-  public void onPickFloor(MouseEvent event) {
-    currentFloor = (int) menu_pickFloor.getValue();
+  public void onPickFloor(ActionEvent event) {
+    int currentFloor = menu_pickFloor.getValue();
     getNodesOnFloor(currentBuilding, currentFloor);
-    fillTable();
   }
 
+  // Called when user clicks "Save" button
   @FXML
   public void onSaveClicked(MouseEvent event) {
-    // nodesTable.
     saveEdits();
   }
 
+  // Called when user clicks "+ Node" button
   @FXML
-  public void onTableRowClicked(MouseEvent event) {
-    nodesTable.scrollTo(0);
+  public void onAddNodeClicked(MouseEvent event) {}
+
+  // Called when user clicks "- Node" button
+  @FXML
+  public void onDelNodeClicked(MouseEvent event) {}
+
+  // Called when user clicks "Next" navigation button
+  @FXML
+  private void onNextClicked(MouseEvent event) throws IOException {
+    trashEdits();
+    Stage stage = (Stage) btn_next.getScene().getWindow();
+    Parent root = FXMLLoader.load(getClass().getResource("downloadMapCSVPage.fxml"));
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
   }
 
+  // Called when user clicks "Back" navigation button
   @FXML
-  public void onCellSelected(Event event) {}
+  private void onBackClicked(MouseEvent event) throws IOException {
+    trashEdits();
+    Stage stage = (Stage) btn_back.getScene().getWindow();
+    Parent root = FXMLLoader.load(getClass().getResource("dataEditor.fxml"));
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  /// Helper methods
 
   private void refreshAllNodes() {
     allNodes.clear();
@@ -81,88 +208,73 @@ public class NodeTableEditorController {
     }
   }
 
-  @FXML
-  private void onNextClicked(MouseEvent event) throws IOException {
-    Stage stage = (Stage) btn_next.getScene().getWindow();
-    Parent root = FXMLLoader.load(getClass().getResource("downloadMapCSVPage.fxml"));
-    Scene scene = new Scene(root);
-    stage.setScene(scene);
-    stage.show();
-  }
-
-  @FXML
-  private void onBackClicked(MouseEvent event) throws IOException {
-    Stage stage = (Stage) btn_back.getScene().getWindow();
-    Parent root = FXMLLoader.load(getClass().getResource("dataEditor.fxml"));
-    Scene scene = new Scene(root);
-    stage.setScene(scene);
-    stage.show();
-  }
-
   private void getBuildings() {
     buildings.clear();
     for (TableNode tn : allNodes) {
-      if (!buildings.contains(tn.buildingProperty().getValue())) {
-        buildings.add(tn.buildingProperty().getValue());
-        menu_pickBuilding.getItems().add(tn.buildingProperty().getValue());
+      if (!buildings.contains(tn.getBuilding())) {
+        buildings.add(tn.getBuilding());
+        menu_pickBuilding.getItems().add(tn.getBuilding());
       }
     }
+    menu_pickBuilding.setValue(buildings.getFirst());
     getFloorsInBuilding(buildings.getFirst());
   }
 
   private void getFloorsInBuilding(String building) {
     for (TableNode tn : allNodes) {
-      if (!floors.contains(tn.floorProperty().getValue())) {
-        floors.add(tn.floorProperty().getValue());
-        menu_pickFloor.getItems().add(tn.floorProperty().getValue());
+      if (!floors.contains(tn.getFloor())) {
+        floors.add(tn.getFloor());
+        menu_pickFloor.getItems().add(tn.getFloor());
       }
     }
-    getNodesOnFloor(building, floors.getFirst().intValue());
+    menu_pickFloor.setValue(floors.getFirst());
+    getNodesOnFloor(building, floors.getFirst());
   }
 
   private void getNodesOnFloor(String building, int floor) {
     trashEdits(); // Undo unsaved changes to current floor
     currentNodes.clear();
-    for (DbNode dn : dbController.floorNodes(floor, building)) {
-      TableNode tn = new TableNode(dn);
-      currentNodes.add(tn);
-      // menu_pickNode.getItems().add(tn.getShortName());
+    for (TableNode tn : allNodes) {
+      if (tn.getFloor() == floor && tn.getBuilding().equals(building)) {
+        currentNodes.add(tn);
+      }
     }
+    fillTable();
   }
 
   private void fillTable() {
-
-    TableColumn<TableNode, StringProperty> nameCol = new TableColumn<>("Name");
-    TableColumn<TableNode, StringProperty> longNameCol = new TableColumn<>("Long Name");
-    TableColumn<TableNode, StringProperty> idCol = new TableColumn<>("Node ID");
-    TableColumn<TableNode, StringProperty> typeCol = new TableColumn<>("Type");
-    TableColumn<TableNode, IntegerProperty> xCol = new TableColumn<>("X");
-    TableColumn<TableNode, IntegerProperty> yCol = new TableColumn<>("Y");
-    TableColumn<TableNode, IntegerProperty> floorCol = new TableColumn<>("Floor");
-    TableColumn<TableNode, StringProperty> buildingCol = new TableColumn<>("Building");
-    TableColumn<TableNode, StringProperty> teamCol = new TableColumn<>("Team");
-
-    nameCol.setCellValueFactory(new PropertyValueFactory<TableNode, StringProperty>("shortName"));
-    longNameCol.setCellValueFactory(
-        new PropertyValueFactory<TableNode, StringProperty>("longName"));
-    idCol.setCellValueFactory(new PropertyValueFactory<TableNode, StringProperty>("nodeID"));
-    typeCol.setCellValueFactory(new PropertyValueFactory<TableNode, StringProperty>("nodeType"));
-    xCol.setCellValueFactory(new PropertyValueFactory<TableNode, IntegerProperty>("x"));
-    yCol.setCellValueFactory(new PropertyValueFactory<TableNode, IntegerProperty>("y"));
-    floorCol.setCellValueFactory(new PropertyValueFactory<TableNode, IntegerProperty>("floor"));
-    buildingCol.setCellValueFactory(
-        new PropertyValueFactory<TableNode, StringProperty>("building"));
-    teamCol.setCellValueFactory(
-        new PropertyValueFactory<TableNode, StringProperty>("teamAssigned"));
-
-    nodesTable
-        .getColumns()
-        .setAll(nameCol, longNameCol, idCol, typeCol, xCol, yCol, floorCol, buildingCol, teamCol);
-
     nodesTable.setItems(currentNodes);
   }
 
-  private void saveEdits() {}
+  private void cacheEdit(TableNode edited) {
+    if (editedNodes.size() < 1) {
+      btn_saveChanges.setDisable(false); // Enable Save button
+    }
 
-  private void trashEdits() {}
+    editedNodes.removeIf(
+        tn ->
+            tn.getNodeID()
+                .equals(edited.getNodeID())); // If node was edited previously, delete old value
+    editedNodes.add(edited); // Cache edit
+    lbl_changesSaved.setText(editedNodes.size() + " unsaved"); // Show number of edits to be saved
+  }
+
+  private void saveEdits() {
+    if (editedNodes.size() > 0) {
+      for (TableNode tn : editedNodes) { // Update DB using chached changes to node data
+        tn.UpdateDbNode();
+      }
+      trashEdits(); // Clear list of edits
+      refreshAllNodes(); // Re-load nodes from DB, just in case
+      getBuildings(); // Update menu items
+    }
+  }
+
+  private void trashEdits() {
+    if (editedNodes.size() > 0) {
+      editedNodes.clear();
+      lbl_changesSaved.setText("All changes saved");
+      btn_saveChanges.setDisable(true);
+    }
+  }
 }
